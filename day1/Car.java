a82da5656baa44a3d873de6ecd61302bd3a1b8ff
PavithraPughaz <139908661+PavithraPughaz@users.noreply.github.com>
2023-07-18 23:02:06 +0530

    Add files via upload
    day1 assignments completed

---
+++ day1/Car.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int price=1000000;
+String brandName="Tata";
+char logo='T';
+float rate=13.56f;
+double variant=234.57676757;
+boolean isnew=false;
+long mobile=4566655555l;
+
+System.out.println("logo is :"+logo);
+System.out.println(logo);
+System.out.println(price);
+System.out.println("the brandName is :"+brandName);
+System.out.println(rate+":"+price);
+System.out.println(variant);
+System.out.println(mobile);
+System.out.println(isnew);
+	}
+
+}
---
+++ day1/firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version=100.2f;
+		String brand="firefox";
+		boolean visible=true;
+		int year=1998;
+		char logo='m';
+		System.out.println(version);
+		System.out.println("The brand is:"+brand);
+		System.out.println(visible);
+		System.out.println(year);
+		System.out.println(logo);
+		
+
+	}
+
+}
